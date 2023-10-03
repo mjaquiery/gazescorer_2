ad518d6c10bff1aa50d74d1d10394232b55329be
Matt Jaquiery <matt.jaquiery@dtc.ox.ac.uk>
2023-10-03 16:52:10 +0100

    handle errors

--- L2CS_run_models.py
+++ L2CS_run_models.py
@@ -49,51 +49,54 @@ for label in labels:
                 if not ret:
                     break
                 # Process the frame to extract eyes and save to numpy array
-                frame_results = gaze_pipeline.step(frame)
-                yaw = frame_results.yaw
-                pitch = frame_results.pitch
-                face_count = 1
-                if len(yaw) != 1 or len(pitch) != 1:
-                    face_count = max(len(yaw), len(pitch))
-                    print(f"WARNING: Found {face_count} faces in frame {frame_number} of {video_file}. Using the first entry.")
-                    yaw = yaw[0]
-                    pitch = pitch[0]
+                try:
+                    frame_results = gaze_pipeline.step(frame)
+                    yaw = frame_results.yaw
+                    pitch = frame_results.pitch
+                    face_count = 1
+                    if len(yaw) != 1 or len(pitch) != 1:
+                        face_count = max(len(yaw), len(pitch))
+                        print(f"WARNING: Found {face_count} faces in frame {frame_number} of {video_file}. Using the first entry.")
+                        yaw = yaw[0]
+                        pitch = pitch[0]
 
-                results.loc[len(results)] = [
-                    video_file,
-                    frame_number,
-                    face_count,
-                    float(pitch),
-                    float(yaw),
-                    labels[0 if pitch < 0 else 1],
-                    label
-                ]
-                if save_videos:
-                    os.makedirs(os.path.join(output_dir, label), exist_ok=True)
-                    img = render(frame, frame_results)
-                    cv2.putText(
-                        img,
-                        f"pitch {round(float(results.pitch[0]), 2)}",
-                        (25, 25),
-                        cv2.FONT_HERSHEY_SIMPLEX,
-                        1,
-                        (0, 0, 0),
-                        thickness=2
-                    )
-                    cv2.putText(
-                        img,
-                        f"yaw {round(float(results.yaw[0]), 2)}",
-                        (25, 50),
-                        cv2.FONT_HERSHEY_SIMPLEX,
-                        1,
-                        (0, 0, 0),
-                        thickness=2
-                    )
-                    cv2.imwrite(os.path.join(output_dir, label, f"{video_file}_{frame_number}.png"), img)
+                    results.loc[len(results)] = [
+                        video_file,
+                        frame_number,
+                        face_count,
+                        float(pitch),
+                        float(yaw),
+                        labels[0 if pitch < 0 else 1],
+                        label
+                    ]
+                    if save_videos:
+                        os.makedirs(os.path.join(output_dir, label), exist_ok=True)
+                        img = render(frame, frame_results)
+                        cv2.putText(
+                            img,
+                            f"pitch {round(float(results.pitch[0]), 2)}",
+                            (25, 25),
+                            cv2.FONT_HERSHEY_SIMPLEX,
+                            1,
+                            (0, 0, 0),
+                            thickness=2
+                        )
+                        cv2.putText(
+                            img,
+                            f"yaw {round(float(results.yaw[0]), 2)}",
+                            (25, 50),
+                            cv2.FONT_HERSHEY_SIMPLEX,
+                            1,
+                            (0, 0, 0),
+                            thickness=2
+                        )
+                        cv2.imwrite(os.path.join(output_dir, label, f"{video_file}_{frame_number}.png"), img)
+                except Exception as e:
+                    print(f"ERROR: Failed to process frame {frame_number} of {video_file}: {e}")
                 frame_number += 1
             video.release()
 
-print(f"Processed {video_file_count} videos in input {input_dir}. Time elapsed: {time.time() - label_time} seconds")
+    print(f"Processed {video_file_count} videos in input {input_dir}. Time elapsed: {time.time() - label_time} seconds")
 
 fname = os.path.join(output_dir, "results.csv")
 os.makedirs(os.path.dirname(fname), exist_ok=True)
